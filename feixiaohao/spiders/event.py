6f66689f6059bc6828eef1d8375c9f53d46533ec
admin <1094233110@qq.com>
2020-02-22 13:57:43 +0800

    备份

--- feixiaohao/spiders/event.py
+++ feixiaohao/spiders/event.py
@@ -1,56 +1,61 @@
 # -*- coding: utf-8 -*-
-from scrapy import Spider,Request
-from feixiaohao.db.mysqlhelper import MysqlHelper
-from scrapy.utils.project import get_project_settings
-settings = get_project_settings()
-import pymysql
-import logging
 import json
+import requests
+import sys
+sys.path.append(r"/data/coin-library/py_spider/coin/feixiaohao/")
+from feixiaohao.db.mysqlhelper import MysqlHelper
+from feixiaohao.tools import common
+DB = MysqlHelper()
+import pymysql
+from feixiaohao import settings
 from feixiaohao import items
 
-class EventSpider(Spider):
-    name = 'event'
-    allowed_domains = ['dncapi.bqiapp.com']
-    start_urls = ['http://dncapi.bqiapp.com/']
+def get_code():
+    tableName = "spider_coin_record"
+    mysql_db_detail = MysqlHelper(config={
+        'host': settings.MYSQL_HOST,
+        'port': settings.MYSQL_PORT,
+        'user': settings.MYSQL_USER,
+        'passwd': settings.MYSQL_PWD,
+        'charset': 'utf8',
+        'cursorclass': pymysql.cursors.DictCursor,
+        'db': 'coin_detail'
+    })
+    return mysql_db_detail.dbGet(tableName=tableName, where={'disable': 0},fields=[tableName + '.id', tableName + '.slug', ], limit=100000)
 
-    def __init__(self):
-        self.mysql_db = MysqlHelper()
-        self.event_api = "https://dncapi.bqiapp.com/api/v2/coin/bigevent?coincode=%s&webp=1"
-        self.mysql_db_detail = MysqlHelper(config={
-            'host': settings['MYSQL_HOST'],
-            'port': settings['MYSQL_PORT'],
-            'user': settings['MYSQL_USER'],
-            'passwd': settings['MYSQL_PWD'],
-            'charset': 'utf8',
-            'cursorclass': pymysql.cursors.DictCursor,
-            'db': 'coin_detail'
-        })
 
-    def start_requests(self):
-        # 从小马本地数据库获取最新的任务表
-        tableName = "spider_coin_record"
-        xiaoma_dbres_list = self.mysql_db_detail.dbGet(tableName=tableName, where={'disable': 0},fields=[tableName + '.id', tableName + '.slug',], limit=100000)
-        for xiaoma_dbres in xiaoma_dbres_list:
-            slug = xiaoma_dbres['slug']
-            # slug = "bitcoin"  # test
-            spider_coin_record_id = xiaoma_dbres['id']
-            url = self.event_api % slug
-            yield Request(url=url, callback=self.parse, meta={'spider_coin_record_id': spider_coin_record_id})
-
-            # break
-    def parse(self, response):
-        meta = response.meta
-        spider_coin_record_id = meta["spider_coin_record_id"]
-        json_text = json.loads(response.text)
-        data_list = json_text["data"]
-        for data in data_list:
-            event_item = items.eventItem()
-            event_item["spider_coin_record_id"] = spider_coin_record_id
-            event_item["eventdate"] = data["eventdate"]
-            event_item["title"] = data["title"]
-            # print(event_item)
-            yield event_item
+def parse(restexts,spider_coin_record_id):
+    json_text = json.loads(restexts)
+    data_list = json_text["data"]
+    for data in data_list:
+        event_item = items.eventItem()
+        event_item["spider_coin_record_id"]=spider_coin_record_id
+        event_item["eventdate"]=data["eventdate"]
+        event_item["title"]=data["title"]
+        duplicate_item = {
+            "md5": common.md5str(str(event_item))
+        }
+        res = DB.dbGet("duplicate", duplicate_item, ["id"])
+        if not res:
+            print(event_item,"插入新的事件消息")
+            DB.dbSave('duplicate', duplicate_item)
+            DB.dbSave('event', event_item)
 
 
 
+def main():
+    spider_list = get_code()
+    for spider in spider_list:
+        slug = spider["slug"]
+        spider_coin_record_id = spider["id"]
+        url = "https://dncapi.bqiapp.com/api/v2/coin/bigevent?coincode=%s&webp=1" % slug
+        restexts = common._request(url,"get",{"User-Agent":common.get_randomUa()})
+        parse(restexts,spider_coin_record_id)
+
+        # break
+
+
+
+if __name__=="__main__":
+    main()
 
